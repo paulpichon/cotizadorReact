1ef68e5ad5797a8d0828ea67c008f37c20de5fae
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-15 14:59:18 -0600

    Componentes con PROPS para los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 //componente propio de REACT
 import { useState } from 'react';
+//importat el header
 import Header from "./components/Header";
+//importar el BUTTON
+import Button from "./components/Button";
 
 function App() {
 
@@ -59,19 +62,16 @@ function App() {
 				<Header />
 
 			<div className='flex justify-between my-6'>
-				<button
-					type='button'
-					className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none
-					hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
-					onClick={handleClickDecremento}
-				>-</button>
 
-				<button
-					type='button'
-					className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none
-					hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
-					onClick={handleClickIncremento}
-				>+</button>
+				<Button 
+					operador='-'
+					fn={handleClickDecremento}
+				/>
+				
+				<Button 
+					operador='+'
+					fn={handleClickIncremento}
+				/>
 			</div>
 
 			<input 
