283b02e9878fc09cb16064a5c4d800540c724058
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-15 00:07:17 -0600

    Añadir botones de incremento y decremento en la cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,58 @@ function App() {
 		setCantidad(+e.target.value);
 	}
 
+	//funcion para restar a la cantidad de prestamo
+	function handleClickDecremento() {
+		//operacion para restar la cantidad
+		const valor = cantidad - STEP;
+
+		//validar que la cantidad minima no sea menor a 0
+	if (valor < MIN) {
+		alert('Cantidad no válida');
+		return;
+	}
+
+		//seteamos el setCantidad con el VALOR
+		setCantidad(valor);
+
+	}
+	
+	//funcion para incrementar a la cantidad de prestamo
+	function handleClickIncremento() {
+		//operacion para restar la cantidad
+		const valor = cantidad + STEP;
+
+		//validar que la cantidad minima no sea menor a 0
+	if (valor > MAX) {
+		alert('Cantidad no válida');
+		return;
+	}
+
+		//seteamos el setCantidad con el VALOR
+		setCantidad(valor);
+
+	}
+
+
 	return (
 		<div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
 				<Header />
+
+			<div className='flex justify-between my-6'>
+				<button
+					type='button'
+					className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none
+					hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+					onClick={handleClickDecremento}
+				>-</button>
+
+				<button
+					type='button'
+					className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none
+					hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+					onClick={handleClickIncremento}
+				>+</button>
+			</div>
 
 			<input 
 				type='range' 
