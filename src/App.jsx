dfdb2bc19f3abdc295aca05209b0d10f6f7f1c85
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-14 20:20:09 -0600

    leer el valor del input range

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,13 @@ function App() {
 	//destructuring de un arreglo
 	const [cantidad, setCantidad] = useState(10000);
 
-	console.log( cantidad );
+	//funcion para el onChange
+	function handleChange(e) {
+		//colocando yun signo de mas(+) convertimos el string en un numero
+		//un signo de más(+) es igual a poner parseInt()
+		//tambien se puede poner Number()
+		console.log(+e.target.value);
+	}
 
 	return (
 		<div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
@@ -17,6 +23,7 @@ function App() {
 			<input 
 				type='range' 
 				className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+				onChange={handleChange}
 			/>
 
 		</div>
