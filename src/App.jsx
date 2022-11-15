3e4022aea620c61b197424b9ec351105a6f5fcc1
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-14 19:56:15 -0600

    creacion de input range

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import Header from "./components/Header";
 
 function App() {
 
-  return (
-    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-        <Header />
-    </div>
-  )
+	return (
+		<div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+				<Header />
+
+			<input 
+				type='range' 
+				className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+			/>
+
+		</div>
+	)
 }
 
 export default App
