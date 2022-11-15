dea8c22517145611387aa139191887e1cec34f38
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-15 16:06:38 -0600

    Estilos a resumen de pagos

--- src/App.jsx
+++ src/App.jsx
@@ -108,6 +108,18 @@ function App() {
 				<option value="24">24 meses</option>
 			</select>
 
+			<div className='my-5 space-y-3 bg-gray-50 p-5'>
+				<h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+					Resumen <span className='text-indigo-600'>de pagos </span>
+				</h2>
+
+				<p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
+				<p className='text-xl text-gray-500 text-center font-bold'>Total a pagar</p>
+				<p className='text-xl text-gray-500 text-center font-bold'>Mensualidades</p>
+
+			</div>
+
+
 		</div>
 	)
 }
